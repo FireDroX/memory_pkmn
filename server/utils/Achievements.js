501958a907ba8f5b9188080d6fa12362bb2cb1c8
FireDroX <FireDroX@gmail.com>
2026-05-04 09:51:32 +0200

    feat: add achievements module with various player rewards and hints

---
+++ server/utils/Achievements.js
@@ -0,0 +1,84 @@
+module.exports = [
+  {
+    id: 0,
+    name: "Create an account",
+    description: "This is free.",
+    emoji: "👋",
+    hint: "Why don't you have it ?",
+    rewards: {
+      xp: 10,
+    },
+  },
+  {
+    id: 1,
+    name: "First win",
+    description: "It was not that hard.",
+    emoji: "🎯",
+    hint: "Win your first game.",
+    rewards: {
+      xp: 10,
+    },
+  },
+  {
+    id: 2,
+    name: "Getting better",
+    description: "You're getting good.",
+    emoji: "💪",
+    hint: "Win 5 games.",
+    rewards: {
+      xp: 25,
+    },
+  },
+  {
+    id: 3,
+    name: "Can't stop him",
+    description: "Unstoppable !",
+    emoji: "🔥",
+    hint: "Win 10 games.",
+    rewards: {
+      xp: 50,
+    },
+  },
+  {
+    id: 10,
+    name: "DOUBLE XP !",
+    description: "You made good money...",
+    emoji: "💰",
+    hint: "Win on weekends.",
+    rewards: {
+      xp: 10,
+    },
+  },
+  {
+    id: 100,
+    name: "Zekrom !!",
+    description: "Best poke ever.",
+    emoji:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/644.png",
+    hint: "Find 644 pair.",
+    rewards: {
+      colors: ["color-zekrom"],
+    },
+  },
+  {
+    id: 150,
+    name: "Max level",
+    description: "Level 5 is all you need",
+    emoji: "💯",
+    hint: "Be level max.",
+    rewards: {
+      xp: 0,
+    },
+  },
+  {
+    id: 200,
+    name: "SHINY",
+    description: "You lucky mf..",
+    emoji: "🌟",
+    hint: "Find a shiny pair.",
+    rewards: {
+      xp: 50,
+      colors: ["color-shiny"],
+    },
+  },
+];
